6aef0a0ba2984f190203eca2b6c2a5c248bbc0ec
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-01 10:29:12 -0300

    Adicionando Navbar

---
+++ react/aparecida-jau-site/src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Navbar = () => {
+    return ( 
+
+        <>
+
+            {/* <header>
+
+                <nav>
+
+                    <BrowserRouter>
+                    
+                        <Routes>
+
+                            <Route path='/' element={<Navbar />} >
+
+                                <Route index element={<Home />} />
+
+                                <Route path='historia' element={<Historia />} />
+
+                                <Route path='boletim-informativo' element={<BoletimInformativo />} />
+
+                                <Route path='contato' element={<Contato />} />
+
+                            </Route>
+
+                        </Routes>
+
+                    </BrowserRouter>
+
+                    <ul>
+                        <li><a href='#'></a></li>
+                        <li><a href='#'></a></li>
+                        <li><a href='#'></a></li>
+                        <li><a href='#'></a></li>
+                    </ul>
+
+                </nav>
+
+            </header> */}
+
+        </> 
+
+    );
+}
+ 
+export default Navbar;
