737116faf9882700a6067c1d2cafedffd74698c2
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-22 10:42:50 -0300

    Adicionando React ao projeto

---
+++ react/aparecida-jau-site/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+
+    <>
+
+      <div>
+        
+        Página inicial
+
+      </div>
+      
+    </>
+
+  )
+
+}
+
+export default App;
