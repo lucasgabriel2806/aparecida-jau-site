5ccb78e028a8ce987998eb4c0fedc048171fa08c
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-08-05 08:12:32 -0300

    Atualizando Inicio

--- aparecida-jau-site/src/app/page.tsx
+++ aparecida-jau-site/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { MiniCard } from "@/components/minicard";
+
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -8,13 +10,45 @@ export default function Home() {
 
   return(
 
-    <div>
+    <>
+
+      <section
+        className='h-[calc(100vh_-_87px)] bg-cover bg-center relative flex items-center justify-center'
+        style={{
+          backgroundImage: "url('/img/inicio/igreja_atual.jpeg')"
+        }}
+      >
+
+        <div className="text-white text-center">
+
+          <h1 className="text-4xl md:text-6xl font-bold uppercase">
+            Paróquia Nossa Senhora Aparecida
+          </h1>
+
+          <div className="mt-4 w-32 mx-auto border-t border-white"></div>
+          <div className="text-2xl mt-2">★</div>
+          <div className="mt-4 w-32 mx-auto border-t border-white"></div>
+
+        </div>
+
+      </section>
 
       <h2 className="font-bold uppercase">Notícias e Eventos</h2>
 
-      
+      <MiniCard 
+        img={'/img/inicio/cards/quarta-cinzas-card.jpg'} 
+        title='Quarta-feira de cinzas' 
+        desc='Início da Quaresma' 
+        alt={'teste'} 
+      />
 
-    </div>
+      <MiniCard 
+        img={'/img/inicio/cards/procissao-domingo-pascoa-card.jpg'} 
+        title='Domingo de Páscoa' 
+        desc='Procissão às 06:00h' 
+        alt={'teste'}/>
+
+    </>
 
   )
 
--- aparecida-jau-site/src/components/minicard/index.tsx
+++ aparecida-jau-site/src/components/minicard/index.tsx
@@ -1,17 +1,35 @@
-export function MiniCard() {
+import Image from "next/image";
+
+export function MiniCard({ img, title, desc, alt }) {
 
     return (
 
         <>
         
             {/* Card */}
-            <div>
+            <div className='w-[288px] h-[318px] p-3 border-2 border-gray-500 rounded-xl'>
 
                 {/* Imagem */}
-                <div></div>
+                <div className=''>
+
+                    <Image 
+                      className='mx-auto rounded-t-xl pb-3'
+                      src={img}
+                      width={260}
+                      height={180}
+                      alt={alt}  
+                    />
+
+                </div>
 
                 {/* Texto */}
-                <div></div>
+                <div>
+
+                    <h3 className="font-bold">{title}</h3>
+
+                    <p>{desc}</p>
+
+                </div>
 
             </div>
 
