68302db2acc79ca2b8e154cd39aee77b6d092fd5
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-08-07 09:53:47 -0300

    Atualizando footer

--- aparecida-jau-site/src/components/footer/index.tsx
+++ aparecida-jau-site/src/components/footer/index.tsx
@@ -8,14 +8,14 @@ export function Footer() {
 
         <footer>
 
-            <div>
+            <div className="flex justify-evenly flex-wrap bg-[#2C3E50] text-white">
 
-                <div>
+                <div className="border-2 border-black">
                     <h2>Localização</h2>
-                    <p>R. Vinte e Quatro de Maio Vila Nova Jau</p>
+                    <address>R. Vinte e Quatro de Maio Vila Nova Jau</address>
                 </div>
 
-                <div>
+                <div className="border-2 border-black">
                     <h2>Redes sociais</h2>
                     <div>
                         <FaYoutube />
@@ -25,27 +25,32 @@ export function Footer() {
                     </div>
                 </div>
 
-                <div>
+                <div className="border-2 border-black">
 
                     <h2>Páginas</h2>
                     
-                    <Link href="/">
-                        Início
-                    </Link>
-
-                    <Link href="paroquia/historia">
-                        Paróquia
-                    </Link>
-
-                    <Link href="boletim">
-                        Boletim Informativo
-                    </Link>
+                    <nav>
+                        <Link href="/">
+                            Início
+                        </Link>
+                        <Link href="paroquia/historia">
+                            Paróquia
+                        </Link>
+                        <Link href="boletim">
+                            Boletim Informativo
+                        </Link>
+                        <Link href="contato">
+                            Contato
+                        </Link>
+                    </nav>
 
                 </div>
 
             </div>
 
-            <div></div>
+            <div>
+                <small>&copy;2025 - Paróquia Nossa Senhora Aparecida</small>
+            </div>
 
         </footer>
 
