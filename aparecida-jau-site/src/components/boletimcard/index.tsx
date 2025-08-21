072e96c06511f62be16d94823c04cbf2f9b4f85e
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-08-21 12:36:24 -0300

    Atualizando BoletimCard

--- aparecida-jau-site/src/components/boletimcard/index.tsx
+++ aparecida-jau-site/src/components/boletimcard/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 import Image from 'next/image';
 
 function BoletimCard() {
 
   return (
 
-    <div className='w-[260px] h-[440px] border-2 border-black rounded-[10px]'>
-
-        {/* <Image
-          src={}  
-        /> */}
-
-    </div>
+    <Link href='/'>
+      <div
+        className='
+        w-[260px] h-[440px] border-2 border-black rounded-[10px]
+        p-2'
+      >
+          <Image
+            className='rounded'
+            src={'/img/boletim/2023/abril.jpg'}
+            alt={'teste'}
+            width={260}
+            height={440}
+          />
+          <div>
+            <h3>Mês</h3>
+            <p>Descrição?</p>
+          </div>
+      </div>
+    </Link>
 
   )
 
