f97b6881f31eff3e9434e39e70a37538f2045527
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-08-25 11:03:26 -0300

    Atualizações gerais | Navbar responsiva

--- aparecida-jau-site/src/components/header/index.tsx
+++ aparecida-jau-site/src/components/header/index.tsx
@@ -1,4 +1,6 @@
-import { FaAngleDown } from "react-icons/fa";
+"use client";
+
+import { FaAngleDown, FaBars } from "react-icons/fa";
 
 import './style.css';
 
@@ -10,75 +12,142 @@ export function Header() {
 
     return(
 
-        // Adicionar no futuro: sticky top-0 z-[9999]
-        <header className="flex bg-[#fff] px-2 py-4 border-b-2 border-black">
+        <>
 
-            <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
+            {/* Adicionar no futuro: sticky top-0 z-[9999] */}
+            <header className="flex bg-[#fff] px-2 py-4 border-b-2 border-black">
 
-                <div>
-                    <Link href='/'>
+                <div className="flex items-center justify-between w-full mx-auto max-w-7xl">
 
-                        {/* Consertar logo do header com margin extra */}
-                        <Image 
-                            className='m-0'
-                            src='/img/logo/aparecida-no-bg.png'
-                            width={270}
-                            height={78}
-                            alt='logo'
-                        />
+                    <div>
+                        <Link href='/'>
 
-                    </Link>
+                            {/* Consertar logo do header com margin extra */}
+                            <Image 
+                                className='m-0'
+                                src='/img/logo/aparecida-no-bg.png'
+                                width={270}
+                                height={78}
+                                alt='logo'
+                            />
+
+                        </Link>
+                    </div>
+
+                    <nav className=''>
+
+                        <div 
+                            className="
+                            hidden mr-6
+                            
+                            max-md:block"
+
+                            onClick={() => {
+                                document.getElementById('nav-links-mobile')?.classList.toggle('hidden');
+                            }}
+                        >
+                            <FaBars size={30} />
+                        </div>
+
+                        <ul 
+                            className="
+                            flex items-center justify-center gap-10
+
+                            max-md:hidden
+                        ">
+
+                            <li className='font-bold uppercase'>
+                                <Link href='/'>
+                                    Início
+                                </Link>
+                            </li>
+
+                            {/* dropdown */}
+                            <li className='dropdown font-bold uppercase'>
+
+                                {/* dropbtn */}
+                                <Link 
+                                    className="dropbtn flex items-center"
+                                    href='/paroquia/historia'>
+                                    Paróquia
+                                    <FaAngleDown />
+                                </Link>
+
+                                {/* dropdown-content */}
+                                <div className="dropdown-content">
+                                    <Link href="/paroquia/historia">
+                                        História
+                                    </Link>
+                                </div>
+                                
+                            </li>
+
+                            <li className='font-bold uppercase'>
+                                <Link href='/boletim'>
+                                    Boletim
+                                </Link>
+                            </li>
+
+                            <li className='font-bold uppercase'>
+                                <Link href='/contato'>
+                                    Contato
+                                </Link>
+                            </li>
+
+                        </ul>
+                    
+                    </nav>
+                    
                 </div>
 
-                <nav>
+            </header>
 
-                    <ul className="flex items-center justify-center gap-10">
+            {/* Links Mobile */}
+            <ul 
+                id="nav-links-mobile"
+                className="hidden w-full bg-white absolute z-[999] border-b-2 border-black">
 
-                        <li className='font-bold uppercase'>
-                            <Link href='/'>
-                                Início
-                            </Link>
-                        </li>
+                <li className='font-bold uppercase p-5'>
+                    <Link href='/'>
+                        Início
+                    </Link>
+                </li>
 
-                        {/* dropdown */}
-                        <li className='dropdown font-bold uppercase'>
+                {/* dropdown */}
+                <li className='dropdown font-bold uppercase p-5'>
 
-                            {/* dropbtn */}
-                            <Link 
-                                className="dropbtn flex items-center"
-                                href='/paroquia/historia'>
-                                Paróquia
-                                <FaAngleDown />
-                            </Link>
+                    {/* dropbtn */}
+                    <Link 
+                        className="dropbtn flex items-center"
+                        href='/paroquia/historia'>
+                        Paróquia
+                        <FaAngleDown />
+                    </Link>
 
-                            {/* dropdown-content */}
-                            <div className="dropdown-content">
-                                <Link href="/paroquia/historia">
-                                    História
-                                </Link>
-                            </div>
-                            
-                        </li>
+                    {/* dropdown-content */}
+                    <div className="dropdown-content">
+                        <Link href="/paroquia/historia">
+                            História
+                        </Link>
+                    </div>
+                    
+                </li>
 
-                        <li className='font-bold uppercase'>
-                            <Link href='/boletim'>
-                                Boletim
-                            </Link>
-                        </li>
+                <li className='font-bold uppercase p-5'>
+                    <Link href='/boletim'>
+                        Boletim
+                    </Link>
+                </li>
 
-                        <li className='font-bold uppercase'>
-                            <Link href='/contato'>
-                                Contato
-                            </Link>
-                        </li>
+                <li className='font-bold uppercase p-5'>
+                    <Link href='/contato'>
+                        Contato
+                    </Link>
+                </li>
 
-                    </ul>
-                
-                </nav>
-                
-            </div>
+            </ul>
 
-        </header>
+        </>
 
     )
 
